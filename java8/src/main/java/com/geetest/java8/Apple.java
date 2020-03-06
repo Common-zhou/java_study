59a636787a036f0790bfd0648d185cc2bb1370f2
zhoubing <1050015785@qq.com>
2020-03-06 17:22:00 +0800

    初始化仓库

---
+++ java8/src/main/java/com/geetest/java8/Apple.java
@@ -0,0 +1,13 @@
+package com.geetest.java8;
+
+/**
+ * @Description:
+ * @Author: zhou
+ * @Date: 2020/3/6
+ * @Time 17:07
+ */
+public class Apple {
+    public static void main(String[] args) {
+        System.out.println("hello");
+    }
+}
