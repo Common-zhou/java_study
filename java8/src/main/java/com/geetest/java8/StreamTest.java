305a6a45b847f6b7d6dba4111cd847ce65d35df5
zhoubing <1050015785@qq.com>
2020-03-07 23:24:22 +0800

    distinct和limit使用

--- java8/src/main/java/com/geetest/java8/StreamTest.java
+++ java8/src/main/java/com/geetest/java8/StreamTest.java
@@ -2,6 +2,7 @@ package com.geetest.java8;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Stream;
 
 import static java.util.stream.Collectors.toList;
 
@@ -31,5 +32,29 @@ public class StreamTest {
             return dish.getName();
         })).limit(3).collect(toList());
         System.out.println(collect);
+
+        //筛选  filter
+        List<Dish> vegetarianMenu = menu.stream().filter(Dish::isVegetarian).collect(toList());
+        System.out.println(vegetarianMenu);
+
+        //切片 limit
+        List<Dish> limitMenu = menu.stream().limit(3).collect(toList());
+        System.out.println(limitMenu);
+
+        System.out.println("======================");
+        //筛选各异的元素
+        Stream.of(1,2,3,1,1,2,2,4,8,9).distinct().forEach(System.out::println);
+        System.out.println("======================");
+        List<Dish> menu2 = Arrays.asList(
+                new Dish("pork", false, 800, Dish.Type.MEAT),
+                new Dish("pork", false, 800, Dish.Type.MEAT),
+                new Dish("chicken", false, 400, Dish.Type.MEAT));
+        //distinct底层使用的Set
+        List<Dish> dishList = menu2.stream().distinct().collect(toList());
+        System.out.println(dishList);
+
+        //跳过元素
+        List<Dish> dishes = menu.stream().skip(5).collect(toList());
+        System.out.println(dishes);
     }
 }
