19eb10d312d9e0cf44e5cdefd509868aaad3050d
zhoubing <1050015785@qq.com>
2020-03-25 22:17:56 +0800

    学习guava的Strings

--- java8/src/test/java/com/geetest/guava/utilities/TestStrings.java
+++ java8/src/test/java/com/geetest/guava/utilities/TestStrings.java
@@ -1,7 +1,11 @@
 package com.geetest.guava.utilities;
 
+import com.google.common.base.CharMatcher;
+import com.google.common.base.Charsets;
 import com.google.common.base.Strings;
 import org.junit.Test;
+
+import java.nio.charset.Charset;
 
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
@@ -14,11 +18,44 @@ import static org.hamcrest.MatcherAssert.assertThat;
  */
 public class TestStrings {
     @Test
-    public void testStrings(){
+    public void testStrings() {
         //nullToEmpty  如果值是null返回空字符串  否则返回原字符串
         assertThat(Strings.nullToEmpty("hello"), equalTo("hello"));
         assertThat(Strings.nullToEmpty(null), equalTo(""));
 
+        assertThat(Strings.emptyToNull("hello"), equalTo("hello"));
+        assertThat(Strings.emptyToNull(null), equalTo(null));
+        assertThat(Strings.emptyToNull(""), equalTo(null));
 
+        assertThat(Strings.isNullOrEmpty(""), equalTo(true));
+        assertThat(Strings.isNullOrEmpty(null), equalTo(true));
+
+        assertThat(Strings.padStart("hello", 3, 'P'), equalTo("hello"));
+        assertThat(Strings.padStart("hello", 10, 'P'), equalTo("PPPPPhello"));
+
+        assertThat(Strings.padEnd("hello", 3, 'p'), equalTo("hello"));
+        assertThat(Strings.padEnd("hello", 10, 'P'), equalTo("helloPPPPP"));
+
+        assertThat(Strings.repeat("hello", 3), equalTo("hellohellohello"));
+//        String repeat = Strings.repeat(null, 3);
+
+        assertThat(Strings.commonPrefix("hello", "heappy"), equalTo("he"));
+        assertThat(Strings.commonSuffix("hello", "hallo"), equalTo("llo"));
+
+
+    }
+
+    @Test
+    public void testCharsets() {
+        Charset charset = Charset.forName("UTF-8");
+        assertThat(Charsets.UTF_8, equalTo(charset));
+    }
+
+    @Test
+    public void testCharMatcher() {
+        assertThat(CharMatcher.javaDigit().matches('5'), equalTo(true));
+        assertThat(CharMatcher.javaDigit().matches('x'), equalTo(false));
+        assertThat(CharMatcher.is('A').countIn("A boy can do everything for a girl"), equalTo(1));
+        
     }
 }
